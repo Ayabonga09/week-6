9213be87b0fab9eee815348fd18468e2a35989a7
Ayabonga09 <booiayabonga26@gmail.com>
2025-08-22 20:38:25 +0200

    m

--- script.js
+++ script.js
@@ -42,8 +42,16 @@ if (contactForm) {
   });
 }
 
-// --- Dark/Light Mode Toggle ---
 const toggleBtn = document.getElementById("theme-toggle");
-toggleBtn?.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  
+  if (body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "Switch to Light Mode";
+  } else {
+    toggleBtn.textContent = "Switch to Dark Mode";
+  }
 });
+
