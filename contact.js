7533e46366e93c65e175736adcc20b5ba47fc4fb
Ayabonga09 <booiayabonga26@gmail.com>
2025-08-31 22:40:22 +0200

    update

--- contact.js
+++ contact.js
@@ -1,8 +1,6 @@
-// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDkGFA3no9bsuQrt5xQc_cyXS5_3Ybmbss",
   authDomain: "ayabonga-portfolio.firebaseapp.com",
@@ -13,11 +11,9 @@ const firebaseConfig = {
   appId: "1:1038329172569:web:0e243651ec031b155c9748"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// Handle form submission
 document.getElementById("contact-form").addEventListener("submit", function (e) {
   e.preventDefault();
 
